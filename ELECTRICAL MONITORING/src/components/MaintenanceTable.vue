c44fbeb8b38dba542b7560c1f23b586b951fa223
Neil Roble <Neilbenjaminroble@gmail.com>
2023-09-14 17:09:37 +0800

    naa nay modal para sa request info entry

--- ELECTRICAL MONITORING/src/components/MaintenanceTable.vue
+++ ELECTRICAL MONITORING/src/components/MaintenanceTable.vue
@@ -1,105 +1,145 @@
 <template>
-    <div class="q-pa-md q-gutter-sm">
-      <q-btn flat rounded color="primary" label="Add Project" />
+
+  <div class="q-pa-sm">
+    <div class="q-pa-sm  " >
+        <q-btn flat rounded color="primary" label="Add Project" icon="add" @click="prompt = true"/>
+    </div>
+      <q-markup-table>
+        <thead>
+          <tr>
+            <th class="text-left">LOCATION</th>
+            <th class="text-center">REQUEST DESCRIPTION</th>
+            <th class="text-center">REQUESTEE</th>
+            <th class="text-center">ASSIGNED GROUP</th>
+            <th class="text-center">CLUSTER</th>
+            <th class="text-center">REMARKS</th>
+            <th class="text-center">ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-left">Frozen Yogurt</td>
+            <td class="text-center">159</td>
+            <td class="text-center">6</td>
+            <td class="text-center">24</td>
+            <td class="text-center">4</td>
+            <td class="text-center">87</td>
+            <td class="text-center">
+              <q-btn color="primary" icon="edit" />&nbsp;
+              <q-btn color="red" icon="delete"  />
+            </td>
+          </tr>
+
+
+        </tbody>
+      </q-markup-table>
     </div>
 
-<q-table>
-  <thead>
-    <tr>
-      <th class="text-left">PROJECT NAME / LOCATION / REFERENCE</th>
-      <th class="text-right">MATERIAL COST</th>
-      <th class="text-right">MATERIAL BALANCE</th>
-      <th class="text-right">CONTINGENCY</th>
-      <th class="text-right">TOTAL PROJECT COST</th>
-      <th class="text-right">NO. DAYS</th>
-      <th class="text-right">DATE STARTED</th>
-      <th class="text-right">TARGET DATE ACCOMPLISHED</th>
-      <th class="text-right">ACTIONS</th>
+    <div>
+      <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 650px">
+        <q-card-section>
+          <div class="text-h6">Request Information</div>
+        </q-card-section>
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="text-left">Frozen Yogurt</td>
-      <td class="text-right">159</td>
-      <td class="text-right">6</td>
-      <td class="text-right">24</td>
-      <td class="text-right">4</td>
-      <td class="text-right">87</td>
-      <td class="text-right">3.9</td>
-      <td class="text-right">327</td>
-      <td class="text-right">
-        <q-btn color="primary" icon="edit" /> &nbsp;
-        <q-btn color="red" icon="delete"  />
-      </td>
-    </tr>
-    <tr>
-      <td class="text-left">Ice cream sandwich</td>
-      <td class="text-right">237</td>
-      <td class="text-right">9</td>
-      <td class="text-right">37</td>
-      <td class="text-right">4.3</td>
-      <td class="text-right">129</td>
-      <td class="text-right">3.9</td>
-      <td class="text-right">327</td>
-      <td class="text-right">
-        <q-btn color="primary" icon="edit" />
-        <q-btn color="red" icon="delete"  />
-      </td>
-    </tr>
-    <tr>
-      <td class="text-left">Eclair</td>
-      <td class="text-right">262</td>
-      <td class="text-right">16</td>
-      <td class="text-right">23</td>
-      <td class="text-right">6</td>
-      <td class="text-right">337</td>
-      <td class="text-right">3.9</td>
-      <td class="text-right">327</td>
-      <td class="text-right">
-        <q-btn color="primary" icon="edit" />
-        <q-btn color="red" icon="delete"  />
-      </td>
-    </tr>
-    <tr>
-      <td class="text-left">Cupcake</td>
-      <td class="text-right">305</td>
-      <td class="text-right">3.7</td>
-      <td class="text-right">67</td>
-      <td class="text-right">4.3</td>
-      <td class="text-right">413</td>
-      <td class="text-right">3.9</td>
-      <td class="text-right">327</td>
-      <td class="text-right">
-        <q-btn color="primary" icon="edit" />
-        <q-btn color="red" icon="delete"  />
-      </td>
-    </tr>
-    <tr>
-      <td class="text-left">Gingerbread</td>
-      <td class="text-right">356</td>
-      <td class="text-right">16</td>
-      <td class="text-right">49</td>
-      <td class="text-right">3.9</td>
-      <td class="text-right">327</td>
-      <td class="text-right">3.9</td>
-      <td class="text-right">327</td>
-      <td class="text-right">
-        <q-btn color="primary" icon="edit" />
-        <q-btn color="red" icon="delete"  />
-      </td>
-    </tr>
+        <q-separator></q-separator>
 
-  </tbody>
-</q-table>
+        <q-card-section>
+          <div class="q-pa-xs">
+               <div class="q-gutter-md row ">
+                    <div class="col">
+                      <q-input v-model="text" label="Request Description" />
+                    </div>
 
-</template>
+              </div>
+              <div class="q-gutter-md row ">
+                  <div class="col">
+                       <q-input v-model="text" label="Requested By" />
+                  </div>
+                  <div class="col">
+                    <q-input v-model="text" label="Contact No." />
+                  </div>
 
-<script>
-import { defineComponent } from 'vue'
+              </div>
+              <div class="q-gutter-md row ">
+                  <div class="col">
+                       <q-select v-model="model" :options="options" label="Assigned Group" />
+                  </div>
+                  <div class="col">
+                    <q-select v-model="model" :options="options" label="Cluster" />
+                  </div>
 
-export default defineComponent({
+              </div>
+              <div class="q-gutter-md row ">
+                  <div class="col">
+                    <q-select v-model="model" :options="options" label="Barangay" />
+                  </div>
+                  <div class="col">
+                    <q-select v-model="model" :options="options" label="Purok" />
+                  </div>
+                  <div class="col">
+                    <q-input v-model="text" label="Street" />
+                  </div>
+              </div>
 
-})
+              <div class="q-gutter-md row ">
+                  <div class="col">
+                    <q-input v-model="text" label="Remarks" type="textarea" />
+                  </div>
 
-</script>
+              </div>
+
+
+
+          </div>
+        </q-card-section>
+
+
+          <!-- <q-input dense v-model="address" autofocus @keyup.enter="prompt = false" /> -->
+        <q-card-actions align="right" >
+          <q-btn color="red" label="Cancel" v-close-popup />
+          <q-btn color="primary" label="Save" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    </div>
+
+  </template>
+
+  <script>
+  import { defineComponent, ref } from 'vue'
+
+  export default {
+    setup(){
+      return {
+        alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+
+      address: ref(''),
+      model: ref(null),
+      options: [
+        'B1 - LSB',
+        'B2 - BLDG MAINTENANCE',
+        'B3 - CLEARING',
+        'B4 - RAC MAINTENANCE',
+        'B5 - COMMUNICATIONS/TELEPHON CABLE -RROW',
+        'B6 - STREET LIGHTS',
+        'B7 - OTHERS',
+        ],
+      Clusters :[
+        'B61',
+        'B62',
+      ],
+      subClusters :[
+        
+      ]
+
+      }
+    }
+  }
+
+
+
+  </script>
