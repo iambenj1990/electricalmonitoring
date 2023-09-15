01468d67d7aa96aad3b680672641ecbe65671f13
Neil Roble <Neilbenjaminroble@gmail.com>
2023-09-15 17:06:20 +0800

    Q-OPTION FUNCTIONAL

--- ELECTRICAL MONITORING/src/components/MaintenanceTable.vue
+++ ELECTRICAL MONITORING/src/components/MaintenanceTable.vue
@@ -1,15 +1,15 @@
 <template>
-
-  <div class="q-pa-sm">
-    <div class="q-pa-sm  " >
-        <q-btn flat rounded color="primary" label="Add Project" icon="add" @click="prompt = true"/>
-    </div>
+  <div>
+         <div class="q-pa-sm">
+              <div class="q-pa-sm  " >
+                    <q-btn flat rounded color="primary" label="Add Project" icon="add" @click="prompt = true"/>
+                </div>
       <q-markup-table>
         <thead>
           <tr>
             <th class="text-left">LOCATION</th>
             <th class="text-center">REQUEST DESCRIPTION</th>
-            <th class="text-center">REQUESTEE</th>
+            <th class="text-center">REQUESTED BY</th>
             <th class="text-center">ASSIGNED GROUP</th>
             <th class="text-center">CLUSTER</th>
             <th class="text-center">REMARKS</th>
@@ -25,8 +25,8 @@
             <td class="text-center">4</td>
             <td class="text-center">87</td>
             <td class="text-center">
-              <q-btn color="primary" icon="edit" />&nbsp;
-              <q-btn color="red" icon="delete"  />
+              <q-btn color="primary" icon="visibility" @click="prompt = true" />&nbsp;
+              <q-btn color="red" icon="delete"   />
             </td>
           </tr>
 
@@ -39,7 +39,7 @@
       <q-dialog v-model="prompt" persistent>
       <q-card style="min-width: 650px">
         <q-card-section>
-          <div class="text-h6">Request Information</div>
+          <div class="text-h6">Maintenance Request Information</div>
         </q-card-section>
 
         <q-separator></q-separator>
@@ -48,43 +48,46 @@
           <div class="q-pa-xs">
                <div class="q-gutter-md row ">
                     <div class="col">
-                      <q-input v-model="text" label="Request Description" />
+                      <q-input v-model="RequestDesc" label="Request Description" />
                     </div>
 
               </div>
               <div class="q-gutter-md row ">
                   <div class="col">
-                       <q-input v-model="text" label="Requested By" />
+                       <q-input v-model="RequestedBy" label="Requested By" />
                   </div>
                   <div class="col">
-                    <q-input v-model="text" label="Contact No." />
-                  </div>
-
-              </div>
-              <div class="q-gutter-md row ">
-                  <div class="col">
-                       <q-select v-model="model" :options="options" label="Assigned Group" />
-                  </div>
-                  <div class="col">
-                    <q-select v-model="model" :options="options" label="Cluster" />
+                    <q-input v-model="ContactNo" label="Contact No." />
                   </div>
 
               </div>
               <div class="q-gutter-md row ">
                   <div class="col">
-                    <q-select v-model="model" :options="options" label="Barangay" />
+                       <q-select v-model="AssignedGroup" :options="options" label="Assigned Group" />
                   </div>
                   <div class="col">
-                    <q-select v-model="model" :options="options" label="Purok" />
+                    <q-select v-model="selectedSubItem" :options="subItemsForSelectedMainItem" label="Cluster" />
                   </div>
                   <div class="col">
-                    <q-input v-model="text" label="Street" />
+                    <q-select v-model="Cluster" :options="subItemForSelectedThirdItem" label="Cluster Description" />
+                  </div>
+
+              </div>
+              <div class="q-gutter-md row ">
+                  <div class="col">
+                    <q-select v-model="Barangay" :options="options" label="Barangay" />
+                  </div>
+                  <div class="col">
+                    <q-select v-model="Purok" :options="options" label="Purok" />
+                  </div>
+                  <div class="col">
+                    <q-input v-model="Street" label="Street" />
                   </div>
               </div>
 
               <div class="q-gutter-md row ">
                   <div class="col">
-                    <q-input v-model="text" label="Remarks" type="textarea" />
+                    <q-input v-model="Remarks" label="Remarks" type="textarea" />
                   </div>
 
               </div>
@@ -104,6 +107,8 @@
     </q-dialog>
 
     </div>
+  </div>
+
 
   </template>
 
@@ -111,15 +116,21 @@
   import { defineComponent, ref } from 'vue'
 
   export default {
-    setup(){
-      return {
-        alert: ref(false),
-      confirm: ref(false),
-      prompt: ref(false),
+    data(){
+       return{
+        RequestDesc: "",
+        ContactNo: "",
+        RequestedBy:"",
+        Remarks:"",
+        Barangay: "",
+        Purok: "",
+        Street: "",
 
-      address: ref(''),
-      model: ref(null),
-      options: [
+        AssignedGroup: null,
+        selectedSubItem: null,
+        Cluster: null,
+
+        options: [
         'B1 - LSB',
         'B2 - BLDG MAINTENANCE',
         'B3 - CLEARING',
@@ -128,14 +139,31 @@
         'B6 - STREET LIGHTS',
         'B7 - OTHERS',
         ],
-      Clusters :[
-        'B61',
-        'B62',
-      ],
-      subClusters :[
-        
-      ]
+        subCluster: {
+          'B6 - STREET LIGHTS' : ['B61 - CLUSTER 1','B62 - CLUSTER 2']
+        },
+        thirdItems: {
+          'B61 - CLUSTER 1':  ['B611 - METERED' , 'B612 - FLATRATE' , 'B613 - SOLAR'],
+          'B62 - CLUSTER 2':  ['B621 - METERED' , 'B622 - FLATRATE' , 'B623 - SOLAR'],
+        }
+       }
+    },
+    setup(){
+      return {
+        alert: ref(false),
+        confirm: ref(false),
+        prompt: ref(false),
+        address: ref(''),
+        model: ref(null),
+      }
+    },
 
+    computed: {
+      subItemsForSelectedMainItem() {
+          return this.AssignedGroup ? this.subCluster[this.AssignedGroup] : [];
+      },
+      subItemForSelectedThirdItem() {
+          return this.selectedSubItem ? this.thirdItems[this.selectedSubItem] : [];
       }
     }
   }
