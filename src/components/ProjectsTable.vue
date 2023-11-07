947ee9f161c3358ec2f804954148958fe0d3e304
Neil Roble <Neilbenjaminroble@gmail.com>
2023-11-07 17:09:36 +0800

    starting electrical project module

---
+++ src/components/ProjectsTable.vue
@@ -0,0 +1,62 @@
+<template>
+
+<div class="q-pa-sm">
+  <div class="q-pa-sm  " >
+      <q-btn flat rounded color="primary" label="Add Project" icon="add" />
+  </div>
+   <div>
+    <q-table
+                title="Electrical Projects List"
+                :rows="ElectricalProject.Projects"
+                :columns="columns"
+
+    />
+   </div>
+  </div>
+</template>
+
+<script>
+import {  ref } from 'vue'
+import {useProjectInfoStore} from 'stores/projectStore.js'
+import { usePhAddress } from "stores/TagumAddressStore.js"
+
+
+
+export default {
+  data() {
+    return {
+        eProjects:[],
+        StatusSelections :[ "Recieved","On-going","Finished"],
+
+
+
+
+
+        columns:[
+                {name: 'ID', label: 'ID', field: '_id', sortable: true, align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' },
+                {name: 'ProjectName', label: 'Project Name', field: 'ProjectName', sortable: true, align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' },
+                {name: 'ReferenceNo', label: 'Reference No', field: 'ReferenceNo', sortable: true, align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' },
+                {name: 'TotalProjectCost', label: 'Total Project Cost', field: 'TotalProjectCost',  align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' },
+                {name: 'ProjectIncharge', label: 'Project Incharge', field: 'ProjectIncharge', sortable: true, align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' },
+                {name: 'TargetAccomplishment', label: 'Target Accomplishment', field: 'TargetAccomplishment', align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' },
+                {name: 'Location', label: 'Location', field: 'Location', align: 'left', headerClasses: 'bg-grey-7 text-white', headerStyle: 'font-size: 1.2 em' }
+
+        ],
+    }
+
+  },
+
+  setup() {
+    const ElectricalProject = useProjectInfoStore()
+    const Brgystore = usePhAddress()
+
+    ElectricalProject.getProjects()
+    console.log( "Prject Data list ==> ",ElectricalProject.Projects)
+
+  }
+
+
+
+};
+
+</script>
---
+++ src/stores/projectStore.js
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const useProjectInfoStore = defineStore('ProjectInfoStore', {
+  state: () => ({
+    Projects:[],
+    Project:{},
+  }),
+
+  actions: {
+    async getProjects(){
+      try {
+        const result = await axios.get('http://10.0.1.23:5000/api/eprojects/');
+        this.Projects = result.data;
+      //  console.log("JobOrders=",this.JobOrders)
+
+      } catch (error) {
+        console.log(`Error fetching Request list : ${error}`)
+      }
+    },
+
+    async getProject(val){
+      try {
+        const result = await axios.get(`http://10.0.1.23:5000/api/eprojects/${val}`);
+        this.Project = result.data;
+      } catch (error) {
+        console.log(`Error fetching Request : ${error}`)
+      }
+    },
+
+    async newProjects(payload){
+      try {
+          const result = await axios.post('http://10.0.1.23:5000/api/eprojects/new', payload);
+          this.Project.push(result.data);
+      } catch (error) {
+        console.log(`Error! unable to save Request : ${error}`);
+      }
+    },
+
+    async UpdateProjects(id,payload){
+      try {
+         // '/requests/:id/update'
+          const result = await axios.put(`http://10.0.1.23:5000/api/eprojects/update/`+ id, payload);
+         // console.log('result====> ',result)
+          // const cursor = this.JobOrder.findIndex((e) => e._id===payload._id);
+          // if (cursor !== -1){
+          //   this.JobOrder[cursor]= result.data;
+          // }
+          this.Project = result.data;
+
+      } catch (error) {
+        console.log(`Error! unable to Update Request : ${error}`);
+      }
+    },
+
+
+  },
+
+
+
+});
